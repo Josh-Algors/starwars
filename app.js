1959b207b36b42c5cdacb0185b2b655cbee42f98
JoshDev <olukoyajoshua72@gmail.com>
2023-04-17 11:38:27 +0100

    changes

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Internal Server Error' });
 });
 
-app.listen(4040, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server started on port 3000');
-});
+})
