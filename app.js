cbcf1b16003459becdb8cb60f8eda410edf88627
JoshDev <olukoyajoshua72@gmail.com>
2023-04-17 11:53:05 +0100

    changes

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const filmRouter = require("./routes/film");
 const commentRouter = require("./routes/comment");
+//Load Environment Variables
+require("dotenv").config();
 
 app.use(express.json());
 app.use('/api/v1', filmRouter);
